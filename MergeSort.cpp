2cfa709ee1bb5339034d62187e8989ead6ff7a6f
SunHaoran2001 <59115396+SunHaoran2001@users.noreply.github.com>
2020-03-02 22:41:19 +0800

    归并排序

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -70,6 +70,8 @@ void Merge(int *A, int left, int mid, int right)
     }
 }
 
+void MergeU2(int *A, int left, int mid, int right);
+void MergeU(int *A, int left, int mid, int right);
 void MergeSort(int *A, int left, int right)
 {
     int mid;
@@ -78,7 +80,51 @@ void MergeSort(int *A, int left, int right)
         mid = (left + right) / 2;
         MergeSort(A, left, mid);
         MergeSort(A, mid + 1, right);
-        Merge(A, left, mid, right);
+        MergeU2(A, left, mid, right);
+    }
+}
+
+void MergeU(int *A, int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = A[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = A[mid + 1 + j];
+    int i = 0, j = 0;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+            A[left++] = L[i++];
+        else
+            A[left++] = R[j++];
+    }
+    while (i < n1)
+        A[left++] = L[i++];
+    while (j < n2)
+        A[left++] = R[j++];
+}
+
+void MergeU2(int *A, int left, int mid, int right)
+{
+    int *B = new int[right - left + 1];
+    int i = left, j = mid + 1, k = 0;
+    while (i <= mid && j <= right)
+    {
+        if (A[i] <= A[j])
+            B[k++] = A[i++];
+        else
+            B[k++] = A[j++];
+    }
+    while (i <= mid)
+        B[k++] = A[i++];
+    while (j <= right)
+        B[k++] = A[j++];
+    for (i = left, k = 0; i <= right; i++)
+    {
+        A[i] = B[k++];
     }
 }
 
@@ -87,7 +133,7 @@ int main()
     int A[100];
     for (int i = 0; i < 5; i++)
         cin >> A[i];
-    MERGE_SORT(A, 0, 4);
+    MergeSort(A, 0, 4);
     for (int i = 0; i < 5; i++)
         cout << A[i] << endl;
     system("pause");
