d055d2a87cb79d361f1324d7ab221fb662cb8f5d
SunHaoran2001 <59115396+SunHaoran2001@users.noreply.github.com>
2020-03-02 18:31:51 +0800

    Sort

---
+++ Insertion_Sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int A[100] = {0, 6, 5, 4, 4, 5, 6};
+int B[100] = {6, 5, 4, 4, 5, 6};
+
+void Insertion_Sort(int len) //下标1开始
+{
+    int key, i;
+    for (int j = 2; j <= len; j++)
+    {
+        key = A[j];
+        i = j - 1;
+        while (i > 0 && A[i] > key)
+        {
+            A[i + 1] = A[i];
+            i = i - 1;
+        }
+        A[i + 1] = key;
+    }
+}
+
+void Insertion_Sort2(int len) //下标0开始
+{
+    int key, i;
+    for (int j = 1; j < len; j++)
+    {
+        key = B[j];
+        i = j - 1;
+        while (i >= 0 && B[i] > key) //降序只需把">"换成"<""
+        {
+            B[i + 1] = B[i];
+            i = i - 1;
+        }
+        B[i + 1] = key;
+    }
+}
+
+int main()
+{
+    Insertion_Sort2(6);
+    for (int i = 0; i < 6; i++)
+        cout << B[i] << " ";
+    return 0;
+}
---
+++ MergeSort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void merge(int *A, int p, int q, int r)
+{
+    int n1 = q - p + 1;
+    int n2 = r - q;
+}
+
+int main()
+{
+
+    system("pause");
+    return 0;
+}
